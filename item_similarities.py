5d131653f59378afccf1ad752ba84bce0b68101b
Evan <evan.rossiter@uwaterloo.ca>
2024-06-18 23:16:46 -0400

    Add files via upload

--- item_similarities.py
+++ item_similarities.py
@@ -1,46 +1,54 @@
 import pandas as pd
 import numpy as np
-import requests
-import json
-import sys
 
-df = pd.read_csv('list_data.csv')
-ids = list(df.columns)
-score_matrix = df.values
+animes = pd.read_csv('short_anime_data.csv')
+ids = [str(ID) for ID in list(set(animes["main_anime"]))]
 
-an_df = pd.read_csv('anime_data.csv')
-anime_dict = an_df[['id', 'title']].set_index('id')['title'].to_dict()
+df = pd.DataFrame().astype(pd.SparseDtype("float", 0))
+for i in range(1, 129):
+    print(i)
+    ndf = pd.read_csv("/Users/evanrossiter/Documents/Code/Anime-Eye/users/%s.csv" % i, index_col = 0)
+    ndf = ndf.astype(pd.SparseDtype("float", 0))
+    df = pd.concat([df, ndf], ignore_index=True)
 
-pr_df = pd.read_csv('more_anime_data.csv')
-prequel_dict = pr_df[['id', 'prequel']].set_index('id')['prequel'].to_dict()
-img_dict = pr_df[['id', 'picture']].set_index('id')['picture'].to_dict()
-eng_dict = pr_df[['id', 'title']].set_index('id')['title'].to_dict()
+ids = list(set(ids) & set(df.columns))
+df = df[ids]
 
-n_cols = score_matrix.shape[1]
+n_cols = len(ids)
 def cosine(v1, v2):
-    non_zero_indices = (v1 > 0) & (v2 > 0)
-    if np.sum(non_zero_indices) < 300:
-        return 1
+    b1, b2 = v1 > 0, v2 > 0
+    non_zero_indices = (b1) & (b2)
+    if np.sum(non_zero_indices) < 50:
+        return 0
     v1, v2 = v1[non_zero_indices], v2[non_zero_indices]
     dot_product = np.dot(v1, v2)
     norm_v1 = np.linalg.norm(v1)
     norm_v2 = np.linalg.norm(v2)
     similarity = dot_product / (norm_v1 * norm_v2)
-    return 1 - similarity
+    return similarity
 
-row_list = []
+def common(v1, v2):
+    b1, b2 = v1 > 0, v2 > 0
+    non_zero_indices = (b1) & (b2)
+    return np.sum(non_zero_indices) / min(np.sum(b1), np.sum(b2))
+
+def mad(v1, v2):
+    b1, b2 = v1 > 0, v2 > 0
+    non_zero_indices = (b1) & (b2)
+    if np.sum(non_zero_indices) < 50:
+        return 0
+    v1, v2 = v1[non_zero_indices], v2[non_zero_indices]
+    return 1 - np.mean(np.abs(v1 - v2)) / 10
+
+distance_matrix = np.zeros((n_cols, n_cols))
 for i in range(n_cols):
-    vec = score_matrix[:, i]
-    distances = np.apply_along_axis(lambda col: cosine(vec, col), axis = 0, arr = score_matrix)
-    recc = np.argsort(distances)
-    recc_ids = [int(ids[int(i)]) for i in recc if not np.isnan(i)]
-    recc_ids = [i for i in recc_ids if i in prequel_dict and prequel_dict[i] == False]
-    recc = [eng_dict[i] for i in recc_ids][:10]
-    pics = [img_dict[i] for i in recc_ids][:10]
-    row_list.append({'ids': recc_ids[:10], 'recommendations': recc, 'pictures': pics})
     print(i)
+    ID = ids[i]
+    vec = df[ID]
+    distances = df.apply(lambda col: mad(vec, col), axis = 0)
+    distance_matrix[i, :] = distances
 
-for i in range(len(row_list)):
-    row_list[i]['id'] = int(ids[i])
-df = pd.DataFrame(row_list)
-df.to_csv("item_neighbours.csv", index = False)
+out = pd.DataFrame(distance_matrix)
+out.index = ids
+out.columns = ids
+out.to_csv("similarities.csv", index = True)
